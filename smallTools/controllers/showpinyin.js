820cb2064e144eb8624a80e783ff4c9058158b94
evewfj <evewfj@qq.com>
2018-10-17 15:55:18 +0800

    remove smallTools

--- smallTools/controllers/showpinyin.js
+++
@@ -1,8 +0,0 @@
-var pinyin = require("pinyin");
-
-module.exports = function(res, pathname) {
-    res.setHeader("Content-Type", "text/html;charset=UTF8");
-    // 拿到/pinyin后面的部分
-    var word = decodeURIComponent(pathname.match(/\/pinyin\/(.+)$/)[1]);
-    res.end(pinyin(word).toString());
-}
